2cf11f50edb97f9c989d35fec878ee36d83486c2
Vidya Nawale <vnawale@digite.com>
2021-06-03 11:35:17 +0530

    Refuel fun added

--- src/main/java/com/digite/kata/Vehicle.java
+++ src/main/java/com/digite/kata/Vehicle.java
@@ -16,11 +16,18 @@ public class Vehicle extends Fuel{
         remainingFuel--;
     }
 
+    public Vehicle refuel(int maxFuel) {
+        Vehicle v = new Vehicle(maxFuel);
+        return v;
+    }
+
     public static void main (String[] arg)
     {
         Vehicle v = new Vehicle(100);
         System.out.println ("remainingFuel: Before: " + v.remainingFuel);
     	v.accelerate();
         System.out.println("remainingFuel: After: " + v.remainingFuel);
+        v = v.refuel(100);
+        System.out.println("remainingFuel: After Refuel: " + v.remainingFuel);
     }
 }
