bc6edd71f23111efa354cdf386c8cec9f45a491d
Vidya Nawale <vnawale@digite.com>
2021-06-03 11:28:29 +0530

    Removed SRP

--- src/main/java/com/digite/kata/Vehicle.java
+++ src/main/java/com/digite/kata/Vehicle.java
@@ -1,33 +1,26 @@
-package com.github.bsferreira.solid.srp.violation;
-package com.digite.kata
+//package com.github.bsferreira.solid.srp.violation;
+package com.digite.kata;
 
-public class Vehicle {
+public class Vehicle extends Fuel{
 
     private final int maxFuel;
     private int remainingFuel;
 
-    public Vehicle(final int maxFuel) {
+    public Vehicle(int maxFuel) {
+        super(maxFuel);
         this.maxFuel = maxFuel;
-        remainingFuel = maxFuel;
-    }
-
-    public void reFuel(){
-        remainingFuel = maxFuel;
-    }
-
-    public int getMaxFuel() {
-        return maxFuel;
-    }
-
-    public int getRemainingFuel() {
-        return remainingFuel;
-    }
-
-    public void setRemainingFuel(final int remainingFuel) {
-        this.remainingFuel = remainingFuel;
+        this.remainingFuel = maxFuel;
     }
 
     public void accelerate() {
         remainingFuel--;
+    }
+
+    public static void main (String[] arg)
+    {
+        Vehicle v = new Vehicle(100);
+        System.out.println ("remainingFuel: Before: " + v.remainingFuel);
+    	v.accelerate();
+        System.out.println("remainingFuel: After: " + v.remainingFuel);
     }
 }
